ce39a4d431ced44b7e9c22ffdca2913bc8c31f73
apidoxySowrov <sowrov.apidoxy@gmail.com>
2025-07-06 13:30:46 +0600

    str variables

---
+++ task/string_variable/string_variable.js
@@ -0,0 +1,8 @@
+const name = "Phul"
+const address = "Chokoria"
+const school = "Center City Model School"
+const fan = "Parsa Evana"
+const movieOfTheYear = "Squid game 3"
+const hobby = "Good Programmer by The GOD Blessed"
+const color = "black"
+console.log(name,address,school,fan,movieOfTheYear,hobby,color)
