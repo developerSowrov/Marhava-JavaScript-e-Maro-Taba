09747e889d781b497880348bc41aec2f34a3d821
apidoxySowrov <sowrov.apidoxy@gmail.com>
2025-07-06 13:43:14 +0600

    boolean_variables

---
+++ task/boolean_variables/booleanVariables.js
@@ -0,0 +1,11 @@
+const happy = false
+const massageByCrush = true
+const celebrating =  true
+const isHappy = true
+const hasMoney = false
+const isHungry = true
+const isStudyDone = false
+const isPhoneCharged = false
+const isDinnerDone = false
+
+console.log(happy,massageByCrush,celebrating,isHappy,hasMoney,isHungry,isStudyDone,isPhoneCharged,isDinnerDone)
