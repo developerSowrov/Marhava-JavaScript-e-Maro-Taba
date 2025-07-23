27b33bfeecc5e24094d3eb96e0279072f7a0a36b
apidoxySowrov <sowrov.apidoxy@gmail.com>
2025-07-24 00:02:02 +0600

    typeof

---
+++ task/let_variable/letVariable.js
@@ -0,0 +1,37 @@
+
+// 1 -------------
+let age = 20
+console.log(age) // 20
+age = 30
+console.log(age) // 30
+
+// 2 --------------
+let pocketMoney = 500
+pocketMoney = 150 
+console.log(pocketMoney) // 150
+
+// 3------------
+let studyTime = 4
+studyTime = 0
+console.log(studyTime) // 0 
+
+// 4 ------------
+let friendsCount = 3
+friendsCount = friendsCount - 2
+console.log(friendsCount) // 1
+
+// 5 ----------------
+let phonePrice = 250000
+phonePrice = 12000
+console.log(phonePrice) // 12000
+
+// 6 --------------
+let chairCount = 40 
+chairCount = chairCount-2 // destroyed two chair 
+chairCount = chairCount - 6 // boro vai niye gace 6 ta 
+console.log(chairCount) // 32
+
+// 7 ----------
+let playTime = 3
+playTime = 0 
+console.log(playTime) // 0
